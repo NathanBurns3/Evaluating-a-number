c25dc18f5b6b5a4a7fec5236af3466561588be71
Nathan Burns <nate10nate@gmail.com>
2023-02-02 18:14:12 -0500

    initial commit

---
+++ lab.js
@@ -0,0 +1,13 @@
+let number = prompt("Enter a number");
+
+let testNumber = 50;
+
+if (number > testNumber) {
+    console.log("Your number is larger than the test number");
+}
+else if (number < testNumber) {
+    console.log("Your number is smaller than the test number");  
+}
+else {
+    console.log("Your number is equal than the test number");
+}
